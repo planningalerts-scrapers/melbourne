7855cd2d06c4d0947134d2053d35040cc1275b01
Matthew Landauer <mlandauer@gmail.com>
2019-05-07 15:36:48 +1000

    Always save record as planningalerts now does versioning

--- scraper.rb
+++ scraper.rb
@@ -57,9 +57,5 @@ all_urls.each do |url|
       raise "Unexpected #{heading}"
     end
   end
-  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true) 
-    ScraperWiki.save_sqlite(['council_reference'], record)
-  else
-    puts "Skipping already saved record " + record['council_reference']
-  end
+  ScraperWiki.save_sqlite(['council_reference'], record)
 end
