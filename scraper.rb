5909026501d308a59de5d0c1a6450887c501c21c
Ian Heggie <ian@heggie.biz>
2025-06-05 23:34:53 +1000

    give more details on what is happening ...

--- scraper.rb
+++ scraper.rb
@@ -17,12 +17,15 @@ begin
   puts "Fetching #{url}"
   p = agent.get(url)
   urls = p.search('table.permits-list .detail .column1 a').map{|a| a["href"]}
+  puts "  found #{urls.size} links"
 
   all_urls += urls
   page += 1
   # FIXME: This is just working around an infinite loop that we currently have
   raise "15 pages processed: aborting due to probably infinite loop" if page == 15
 end until urls.count == 0
+
+puts "No urls found!" if all_urls.empty?
 
 
 all_urls.each do |url|
